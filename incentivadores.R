3337f7fac5a130261a9825524d8b0554667e33a7
Takuji <yohan.takai@gmail.com>
2021-01-26 17:35:32 -0300

    'test'

---
+++ incentivadores.R
@@ -0,0 +1,21 @@
+#libraries
+library(tidyverse)
+library(jsonlite)
+library(RCurl)
+library(httr)
+library(glue)
+library(tidyr)
+
+
+
+
+urls<- as_tibble( seq(100,94300, by = 100)) %>% 
+  mutate(requisicao = glue('http://api.salic.cultura.gov.br/v1/incentivadores/?limit=100&offset={value}'))
+
+urls<- urls %>% 
+  mutate(resposta = map(requisicao, ~fromJSON(.x)),
+         funcionou = map_lgl(resposta, ~ifelse(is.null(.x$error), TRUE, FALSE)))
+
+
+saveRDS(urls, file = "incentivadores.rds")
+
