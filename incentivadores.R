7cfc14473b9ef4a0132679c78d2eb7608ca93394
Takuji <yohan.takai@gmail.com>
2021-01-26 20:19:37 -0300

    'projetos1'

--- incentivadores.R
+++ incentivadores.R
@@ -9,13 +9,29 @@ library(tidyr)
 
 
 
-urls<- as_tibble( seq(100,94300, by = 100)) %>% 
-  mutate(requisicao = glue('http://api.salic.cultura.gov.br/v1/incentivadores/?limit=100&offset={value}'))
+# urls<- as_tibble( seq(0,94300, by = 100)) %>% 
+#   mutate(requisicao = glue('http://api.salic.cultura.gov.br/v1/incentivadores/?limit=100&offset={value}'))
+# 
+# urls<- urls %>% 
+#   mutate(resposta = map(requisicao, ~fromJSON(.x)),
+#          funcionou = map_lgl(resposta, ~ifelse(is.null(.x$error), TRUE, FALSE)))
+# 
+# 
+# saveRDS(urls, file = "incentivadores.rds")
+# 
 
-urls<- urls %>% 
+#103500
+urls <- as_tibble(seq(0,103500,by = 100)) %>% 
+  mutate(requisicao = glue('http://api.salic.cultura.gov.br/v1/projetos/?limit=100&offset={value}'))
+
+urls<- urls %>%
   mutate(resposta = map(requisicao, ~fromJSON(.x)),
          funcionou = map_lgl(resposta, ~ifelse(is.null(.x$error), TRUE, FALSE)))
 
+write.csv2(urls, file = "projetos.csv")
+saveRDS(urls, file = "projetos.rds")
 
-saveRDS(urls, file = "incentivadores.rds")
 
+
+
+fromJSON('http://api.salic.cultura.gov.br/v1/projetos/?limit=100&offset=100&')
